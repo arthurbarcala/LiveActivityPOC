541bb7d3ef48d132b6aaf9dd397a671ca75a8868
arthurbarcala <arthurfbarcalars@gmail.com>
2025-08-04 14:48:30 -0300

    Initial Commit

---
+++ LiveActivityPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveActivityPOC
+//
+//  Created by Arthur Silva on 04/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LiveActivityPOCTests/LiveActivityPOCTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityPOCTests.swift
+//  LiveActivityPOCTests
+//
+//  Created by Arthur Silva on 04/08/25.
+//
+
+import Testing
+@testable import LiveActivityPOC
+
+struct LiveActivityPOCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
